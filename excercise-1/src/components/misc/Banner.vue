8befd681ae7088d60a38dda04b2300f331a9087a
John Arias <john@thinkbitsolutions.com>
2023-02-22 19:17:17 +0800

    Feat: Homepage
    N/A

---
+++ excercise-1/src/components/misc/Banner.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="relative">
+    <div class="banner"></div>
+    <div
+      class="absolute w-full text-center left-[50%] top-[54%] translate-x-[-50%] translate-y-[-50%]"
+    >
+      <h1 class="text-[40px] font-bold text-white">
+        Hello
+        <br class="hidden sm:block" />
+        Developer!
+      </h1>
+      <h2 class="mt-3 text-3xl text-white">
+        Lorem ipsum dolor sit amet,
+        <br class="hidden sm:block" />
+        consectetur adipiscing elit
+      </h2>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.banner {
+  height: 45vh;
+  background: url('@/assets/img/1920x650.png');
+  background-size: 284% 250%;
+  background-position: center;
+  background-repeat: no-repeat;
+  filter: brightness(70%);
+}
+
+@media only screen and (min-width: 1200px) {
+  .banner {
+    background-size: 200% 250%;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .banner {
+    background-size: cover;
+  }
+}
+</style>
---
+++ excercise-1/src/components/misc/Card.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="card w-full rounded-lg border-[1px] border-[#e4e6eb] md:w-[90%] md:m-auto md:mt-[40px]"
+  >
+    <div class="p-[22px]">
+      <figure class="relative bg-[#cccccc]">
+        <img
+          class="rounded"
+          :src="require(`@/assets/img/${cardData.image}`)"
+          :alt="cardData.image"
+        />
+        <p class="absolute right-[10px] bottom-[4px] text-[10px] text-gray-400">
+          {{ cardData.author }}
+        </p>
+      </figure>
+    </div>
+    <div class="card-body pt-0">
+      <p class="mt-4 text-center">{{ cardData.description }}</p>
+      <div class="card-actions justify-center mt-4">
+        <button
+          class="btn min-h-[0] h-[40px] pr-5 pl-5 bg-transparent text-black border-[3px] rounded-full uppercase hover:text-white"
+        >
+          Read More
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// Base Imports
+import { defineProps } from 'vue';
+
+// Set Props
+const cardData = defineProps({
+  image: String,
+  author: String,
+  description: String,
+});
+</script>
